e46dc1a59858b85a02bcd63e02852113eb076b36
eloise <yc6107@nyu.edu>
2024-01-07 17:43:24 +0800

    third commit

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -16,10 +17,28 @@ public class LoginPage implements ActionListener {
     LoginPage(HashMap<String,String> LoginInfoOriginal){
         logininfo = LoginInfoOriginal;
         userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,100,75,25);
+        userPasswordLabel.setBounds(50,150,75,25);
+        messageLAbel.setBounds(125,250,250,35);
+        messageLAbel.setFont(new Font(null,Font.ITALIC,25));
+
+        userIdField.setBounds(125,100,200,25);
+        userPasswordField.setBounds(125,150,200,25);
+        loginButton.setBounds(125,200,100,25);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(this);
+        resetButton.setBounds(225,200,100,25);
+        loginButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
+
 
         frame.add(userIDLabel);
         frame.add(userPasswordLabel);
+        frame.add(messageLAbel);
+        frame.add(userIdField);
+        frame.add(userPasswordField);
+        frame.add(loginButton);
+        frame.add(resetButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
@@ -30,6 +49,29 @@ public class LoginPage implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == resetButton){
+            userIdField.setText("");
+            userPasswordField.setText("");
+        }
+        if (e.getSource() == loginButton){
+            String userID = userIdField.getText();
+            String password = String.valueOf(userPasswordField.getPassword());
+            if(logininfo.containsKey(userID)){
+                if (logininfo.get(userID).equals(password)){
+                    messageLAbel.setForeground(Color.green);
+                    messageLAbel.setText("Login successful");
+                    WelcomePage welcomePage = new WelcomePage();
+                }
+                else{
+                    messageLAbel.setForeground(Color.red);
+                    messageLAbel.setText("Wrong Password");
+                }
+            }
+            else{
+                messageLAbel.setForeground(Color.red);
+                messageLAbel.setText("user not found");
+            }
 
+        }
     }
 }
